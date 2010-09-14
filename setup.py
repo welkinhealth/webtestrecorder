1e0958a6199daa7621b6688934ba1db6a8c8ab3f
Ian Bicking <ianb@colorstudy.com>
2010-09-14 11:27:49 -0500

    documentation etc for a release

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup, find_packages
+import os
 
 version = '0.1'
 
+here = os.path.dirname(os.path.abspath(__file__))
+index = os.path.join(here, 'docs', 'index.txt')
+long_description = open(index).read()
+long_description = long_description.split('split here', 1)[1].strip()
+
 setup(name='WebTestRecorder',
       version=version,
-      description="",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      description="Record activity from a WSGI application, and generate WebTest tests from that record",
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Internet :: WWW/HTTP :: WSGI',
+          'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+          'Topic :: Software Development :: Testing',
+          ],
       keywords='wsgi testing unittest doctest webtest',
       author='Ian Bicking',
       author_email='ianb@colorstudy.com',
@@ -18,7 +30,7 @@ setup(name='WebTestRecorder',
       zip_safe=False,
       install_requires=[
           'WebTest',
-          'WebOb',
+          'WebOb>=1.0',
           'Tempita',
           'WSGIProxy',
       ],
